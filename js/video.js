5ec83259c09bcfdc1cfb57e696993951888ddf5d
Michael Raichelson <i.am@michaelraichelson.com>
2022-08-07 08:45:24 -0500

    04. Motion on Website Challenges: Video JS

--- js/video.js
+++ js/video.js
@@ -1,2 +1,13 @@
-const video = document.querySelector('video')
-video.setAttribute('autoplay', '')
+const motionQuery = matchMedia('(prefers-reduced-motion)')
+
+const handleMotionPreferences = () => {
+	const video = document.querySelector('video')
+	if (motionQuery.matches) {
+		video.setAttribute('controls', '')
+	} else {
+		video.setAttribute('autoplay', '')
+	}
+}
+
+motionQuery.addEventListener('change', handleMotionPreferences)
+handleMotionPreferences()
